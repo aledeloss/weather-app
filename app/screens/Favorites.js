2e635be88b710b853d05c13254094106bb798371
nllopiz <nllopiz@gmail.com>
2021-11-03 21:45:07 -0300

    con lista de Ale y touch para cada list item

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -1,13 +1,44 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { ScrollView, View, TouchableHighlight } from "react-native";
+
+import FavoriteItem from "../components/FavoriteItem";
+import CityWeatherListItem from "../components/CityWeatherListItem";
 
 export default function Favorites() {
-    
-    return(
-        <View>
-            <Text>
-                Mis favoritos...
-            </Text>
-        </View>
-    );
+  const favoritesData = [
+    {
+      id: 0,
+      ciudad: "Mar del Plata",
+      pais: "AR",
+    },
+    {
+      id: 1,
+      ciudad: "Bariloche",
+      pais: "AR",
+    },
+    {
+      id: 2,
+      ciudad: "Miramar",
+      pais: "AR",
+    },
+  ];
+
+  return (
+    <ScrollView>
+      {favoritesData.map((city) => {
+        console.log(city.id, city.ciudad);
+        return (
+          <>
+            <TouchableHighlight
+              activeOpacity={0.6}
+              underlayColor="#DDDDDD"
+              onPress={() => alert("Pressed!")}
+            >
+              {city && <CityWeatherListItem ciudadPais={city} />}
+            </TouchableHighlight>
+          </>
+        );
+      })}
+    </ScrollView>
+  );
 }
