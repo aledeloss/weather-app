b9d905ec5e71ae56b61eceaeb63bee9f3999d915
Verónica Monzón <veronikmonzon@gmail.com>
2021-11-11 17:33:41 -0300

    Add Location

--- app/components/SearchCities.js
+++ app/components/SearchCities.js
@@ -1,102 +1,127 @@
-import React, {useState} from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import {Icon} from "react-native-elements";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useState, useEffect } from "react";
+import MapView, { Marker } from "react-native-maps";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { Icon } from "react-native-elements";
+import * as Permissions from "expo-permissions";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Location from "expo-location";
 
 //GooglePlacesAutocomplete: este componente es el buscador de las ciudades, donde trae todos los datos, latitud,
 //longitud, imagenes, nombre, pais, etc.
 
-const {width, height}=Dimensions.get('window');
-const latitudDelta=10;
-const longitudeDelta=latitudDelta + (width/height);
+const { width, height } = Dimensions.get("window");
+const latitudDelta = 10;
+const longitudeDelta = latitudDelta + width / height;
 
-export default function SearchCities(){
-  const [region, setReagion]=useState({
-    latitude:-34.61315,
-    longitude:-58.37723,
-    latitudeDelta:latitudDelta,
-    longitudeDelta:longitudeDelta,
+export default function SearchCities() {
+  const [region, setRegion] = useState({
+    latitude: -34.61315,
+    longitude: -58.37723,
+    latitudeDelta: latitudDelta,
+    longitudeDelta: longitudeDelta,
   });
-  const [ciudad, setCiudad]=useState('');
+  const [ciudad, setCiudad] = useState([]);
   let ciudades = [];
-  const guardarCiudad = async ()=> {
-    try {     
-        ciudades.push(ciudad);
-        const json_value = JSON.stringify(ciudades);
-        await AsyncStorage.setItem('ciudades', json_value);
-        console.log('Guardar:'+json_value)
-        getData();
+  useEffect(() => {
+    (async () => {
+      const resultPermissions =
+        await Location.requestForegroundPermissionsAsync();
+      console.log(resultPermissions);
+      const statusPermissions = resultPermissions.status;
+
+      if (statusPermissions !== "granted") {
+        alert("Tienes que aceptar los permisos de localizacion");
+      } else {
+        const loc = await Location.getCurrentPositionAsync({});
+        setRegion({
+          latitude: loc.coords.latitude,
+          longitude: loc.coords.longitude,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.001,
+        });
       }
-    catch (e) {
-  
+    })();
+  }, []);
+  const guardarCiudad = async () => {
+    try {
+      ciudades.push(ciudad);
+      const json_value = JSON.stringify(ciudades);
+      await AsyncStorage.setItem("ciudades", json_value);
+      console.log("Guardar:" + json_value);
+      getData();
+    } catch (e) {
       console.log(e);
     }
   };
-  
 
   const getData = async () => {
     try {
-      const jsonValue = await AsyncStorage.getItem('ciudades')
+      const jsonValue = await AsyncStorage.getItem("ciudades");
       return console.log(jsonValue != null ? JSON.parse(jsonValue) : null);
-    } catch(e) {
+    } catch (e) {
       console.log(e);
     }
-  }
-  
+  };
 
-
-    return (
-      <View style={styles.container}>
-      
-        <GooglePlacesAutocomplete 
-        placeholder='Search'
+  return (
+    <View style={styles.container}>
+      <GooglePlacesAutocomplete
+        placeholder="Search"
         fetchDetails={true}
         GooglePlacesDetailsQuery={{
-          rankby:"distance"
+          rankby: "distance",
         }}
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           console.log(data, details);
-          setReagion({
+          setRegion({
             latitude: details.geometry.location.lat,
-            longitude:details.geometry.location.lng,
-            latitudeDelta:latitudDelta,
-            longitudeDelta:longitudeDelta,
-          })
-          setCiudad(details.address_components[0].long_name)
-          
+            longitude: details.geometry.location.lng,
+            latitudeDelta: latitudDelta,
+            longitudeDelta: longitudeDelta,
+          });
+          setCiudad({
+           name: details.address_components[0].long_name,
+           location:details.geometry.location
+          });
         }}
         query={{
-          key: '',
-          language: 'es-419',
-          components: 'country:ar',
-      
+          key: "AIzaSyDZrkPzHejNRtTUoYtY6lxts8a-URSGAiY",
+          language: "es-419",
+          components: "country:ar",
         }}
         styles={{
-          container: { flex: 0, position: "absolute", width: "100%", zIndex: 1},
-          listView: { backgroundColor: "white" }
+          container: {
+            flex: 0,
+            position: "absolute",
+            top:10,
+            width: "85%",
+            zIndex: 1,
+          },
+          listView: { backgroundColor: "white" },
         }}
-      onFail={error => console.error(error)}
+        onFail={(error) => console.error(error)}
       />
-      <MapView style={styles.map}
-        initialRegion ={{
-        latitude:-34.61315,
-        longitude:-58.37723,
-        latitudeDelta:latitudDelta,
-        longitudeDelta:longitudeDelta,
-      }}>
-      <Marker
-       coordinate={{
-         latitude:region.latitude,
-         longitude:region.longitude,
-         latitudeDelta:latitudDelta,
-         longitudeDelta:longitudeDelta,
+      <MapView
+        style={styles.map}
+        showsUserLocation={true}
+        initialRegion={{
+          latitude: -34.61315,
+          longitude: -58.37723,
+          latitudeDelta: latitudDelta,
+          longitudeDelta: longitudeDelta,
         }}
-     
-      />
-  
+      >
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude,
+            latitudeDelta: latitudDelta,
+            longitudeDelta: longitudeDelta,
+          }}
+          draggable
+        />
       </MapView>
       <Icon
         type="material-community"
@@ -107,28 +132,22 @@ export default function SearchCities(){
         onPress={guardarCiudad}
       />
     </View>
-    );
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex:0,
-
+    flex: 1,
   },
   map: {
-
-    width:width,
-    height:height,  
-    zIndex:-1,
+    width: width,
+    height: height,
+    zIndex: -1,
   },
-  button:{
-    position:"absolute",
-    right:20,
-    zIndex:2,
-  }
+  button: {
+    position: "absolute",
+    top:50,
+    right:10,
+    zIndex: 2,
+  },
 });
-    
-	
-
-
-
