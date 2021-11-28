f17e88f024135fb26306e7a275a9ff3ac5c74809
nllopiz <nllopiz@gmail.com>
2021-11-28 18:03:47 -0300

    validar ciudad vacía

--- app/components/SearchCities.js
+++ app/components/SearchCities.js
@@ -20,7 +20,7 @@ export default function SearchCities() {
     longitude: -63.616672,
     latitudeDelta: latitudDelta,
     longitudeDelta: longitudeDelta,
-    name:''
+    name: "",
   });
   const [ciudad, setCiudad] = useState([]);
   let ciudades = [];
@@ -40,47 +40,51 @@ export default function SearchCities() {
           longitude: loc.coords.longitude,
           latitudeDelta: latitudDelta,
           longitudeDelta: longitudeDelta,
-          name:'Estoy aquí',
+          name: "Estoy aquí",
         });
       }
     })();
   }, []);
+
   const guardarCiudad = async () => {
     try {
-      const value = await AsyncStorage.getItem('ciudades');
-      console.log('lo que esta guardado:'+ value);
-      if (value) {
-        ciudades = JSON.parse(value);
-        if (
-          ciudades.find(
-            item =>
-              item.name.trim().toUpperCase() === ciudad.name.trim().toUpperCase(),
-          )
-        ) {
-          return alert('Valor duplicado.');
+      const value = await AsyncStorage.getItem("ciudades");
+      console.log("lo que esta guardado:" + value);
+      console.log(ciudad);
+      if (ciudad.name) {
+        if (value) {
+          ciudades = JSON.parse(value);
+          if (
+            ciudades.find(
+              (item) =>
+                item.name.trim().toUpperCase() ===
+                ciudad.name.trim().toUpperCase()
+            )
+          ) {
+            return alert("Valor duplicado.");
+          } else {
+            ciudades.push(ciudad);
+            const json_value = JSON.stringify(ciudades);
+            await AsyncStorage.setItem("ciudades", json_value);
+            const value1 = await AsyncStorage.getItem("ciudades");
+            console.log("lo que esta guardado 1:" + value1);
+          }
         } else {
           ciudades.push(ciudad);
           const json_value = JSON.stringify(ciudades);
-          await AsyncStorage.setItem('ciudades', json_value);
-          const value1 = await AsyncStorage.getItem('ciudades');
-          console.log('lo que esta guardado 1:'+ value1);
+          await AsyncStorage.setItem("ciudades", json_value);
+          const value2 = await AsyncStorage.getItem("ciudades");
+          console.log("lo que esta guardado 2:" + value2);
         }
-      } else {
-        ciudades.push(ciudad);
-        const json_value = JSON.stringify(ciudades);
-        await AsyncStorage.setItem('ciudades', json_value);
-        const value2 = await AsyncStorage.getItem('ciudades');
-        console.log('lo que esta guardado 2:'+ value2);
-      }
 
-      // navigation.navigate('Home');
-    
+        // navigation.navigate('Home');
+        return null;
+      }
     } catch (e) {
       console.log(e);
     }
   };
 
- 
   return (
     <View style={styles.container}>
       <GooglePlacesAutocomplete
@@ -100,24 +104,24 @@ export default function SearchCities() {
             name: details.address_components[0].short_name,
           });
           setCiudad({
-           id: details.place_id,
-           name: details.address_components[0].long_name,
-           lat: details.geometry.location.lat,
-           lng: details.geometry.location.lng,
-           img: details.photos[0].photo_reference,
+            id: details.place_id,
+            name: details.address_components[0].long_name,
+            lat: details.geometry.location.lat,
+            lng: details.geometry.location.lng,
+            img: details.photos[0].photo_reference,
           });
         }}
         query={{
           key: "AIzaSyDZrkPzHejNRtTUoYtY6lxts8a-URSGAiY",
           language: "es-419",
           components: "country:ar",
-          types:'(cities)',
+          types: "(cities)",
         }}
         styles={{
           container: {
             flex: 0,
             position: "absolute",
-            top:10,
+            top: 10,
             width: "85%",
             zIndex: 1,
           },
@@ -134,7 +138,6 @@ export default function SearchCities() {
           latitudeDelta: latitudDelta,
           longitudeDelta: longitudeDelta,
         }}
-        
       >
         <Marker
           // anchor={{x: 0.5, y: 1}}
@@ -146,9 +149,7 @@ export default function SearchCities() {
             longitudeDelta: longitudeDelta,
           }}
           title={region.name}
-        
         />
-        
       </MapView>
       <Icon
         type="material-community"
@@ -167,14 +168,14 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: width-40,
-    height: height-150,
+    width: width - 40,
+    height: height - 150,
     zIndex: -1,
   },
   button: {
     position: "absolute",
     bottom: 10,
-    right:10,
+    right: 10,
     zIndex: 2,
   },
 });
